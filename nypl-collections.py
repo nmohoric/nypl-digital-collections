15920dc1f8e717db709888ab75af9e45dc336341
Nick Mohoric <nick.mohoric@gmail.com>
2013-04-11 06:45:31 -0400

    Generic search definitions

--- nypl-collections.py
+++ nypl-collections.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import requests
+
 class NYPLsearch:
   def __init__(self,token=None):
     self.token = token or ""
@@ -13,4 +15,17 @@ class NYPLsearch:
 
     self.base = "http://api.repo.nypl.org/api/v1/items"
 
-  
+  def return_captures(self, uuid, withTitle=False, format='json'):
+    # Return the captures for a given uuid 
+    # optional value withTitles=yes
+
+  def return_uuid(self, type, val, format='json'):
+    # Return the item-uuid for a identifier.
+
+  def return_mods(self, uuid, format='json'):
+    # Return a mods record for a given uuid
+
+  def search_mods(self, q, field=None, format='json'):
+    # Search across all (without field) or in specific field 
+    # (valid fields at http://www.loc.gov/standards/mods/mods-outline.html)
+
