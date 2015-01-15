6679bdb26f95e72339dcf2b1b0fc64ccecfb3da5
Nick Mohoric <nick.mohoric@gmail.com>
2015-01-15 11:37:03 -0500

    Fleshed out setup, moved nypl to directory

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(name = "nyplcollections",
     description = "new york public library image collections api",
     author = "nick mohoric",
     author_email = "nick.mohoric@gmail.com",
+    packages = ['nyplcollections'],
+    package_data = {'package' : files },
     long_description = """Really long text here.""" 
 ) 
