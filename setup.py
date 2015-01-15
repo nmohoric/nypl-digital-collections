9720b08363325e884c0bcd66ff130f6d378d7574
Nick Mohoric <nick.mohoric@gmail.com>
2015-01-15 11:51:33 -0500

    Moved directories, fixed setup.py to use new directory

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 
 files = ["nyplcollections.py"]
 
-setup(name = "nyplcollections",
+setup(name = "NYPLsearch",
     version = "1",
     description = "new york public library image collections api",
     author = "nick mohoric",
     author_email = "nick.mohoric@gmail.com",
-    packages = ['nyplcollections'],
+    packages = ['NYPLsearch'],
     package_data = {'package' : files },
+    install_requires=["xmltodict"],
     long_description = """Really long text here.""" 
 ) 
