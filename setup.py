f09b3b4c12e47247e5db300016b34792b853dc41
Nick Mohoric <nick.mohoric@gmail.com>
2015-01-15 11:25:44 -0500

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+files = ["nyplcollections.py"]
+
+setup(name = "nyplcollections",
+    version = "1",
+    description = "new york public library image collections api",
+    author = "nick mohoric",
+    author_email = "nick.mohoric@gmail.com",
+    long_description = """Really long text here.""" 
+) 
