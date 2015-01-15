e4d40014db44420059a347f84a05aa14559907d3
Nick Mohoric <nick.mohoric@gmail.com>
2015-01-15 15:14:38 -0500

    Moved files around

--- nypl-collections/nyplcollections-example.py
+++ nypl-collections/nyplcollections-example.py
@@ -6,12 +6,12 @@ from nyplcollections import NYPLsearch
 # Create search object
 # Could also pass in a format of 'xml' if you want to use the raw_results
 # and require xml. Otherwise the search will return json in raw_results
-searchObj = NYPLsearch('[YOUR_KEY]')
+searchObj = NYPLsearch('dkh183x9ibxj4x8f')
 
 # Find captures based on uuid
 # Don't need to set this equal to anything, could also access results from
 # searchObj.results
-temp = searchObj.captures('2600a3f0-c5ec-012f-424e-58d385a7bc34')
+temp = searchObj.mods('acfeeb2d-7c5e-4ce7-e040-e00a180644aa')
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(temp)
--- tests/testsample.py
+++ tests/testsample.py
@@ -1,2 +1,4 @@
+import NYPLsearch
+
 def test_numbers_3_4():
     assert 3 * 4 == 12 
