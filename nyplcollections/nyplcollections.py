005e202cbaf1a690a1b6d8f5646d29cfa769604a
fitnr <fitnr@fakeisthenewreal>
2015-02-12 12:09:59 -0500

    PEP8 formatting

--- nyplcollections/nyplcollections.py
+++ nyplcollections/nyplcollections.py
@@ -10,61 +10,60 @@ class NYPLsearch():
     except AssertionError:
         print "You will need to use your Authenticaton Token.\n This can be retrieved at http://api.repo.nypl.org/sign_up"
     
-    self.token = token
-    self.page = page
-    self.per_page = per_page
-    self.format = format
-    self.base = "http://api.repo.nypl.org/api/v1/items"
 
+        self.token = token
+        self.page = page
+        self.per_page = per_page
+        self.format = format
+        self.base = "http://api.repo.nypl.org/api/v1/items"
 
-  # Return the captures for a given uuid 
-  # optional value withTitles=yes
-  def captures(self, uuid, withTitles=False):
-    return self._get( '/'.join([self.base, uuid]), 
-        { 'withTitles' : 'yes' if withTitles else 'no' } )
+    # Return the captures for a given uuid
+    # optional value withTitles=yes
+    def captures(self, uuid, withTitles=False):
+        return self._get('/'.join([self.base, uuid]),
+                         {'withTitles': 'yes' if withTitles else 'no'})
 
-  # Return the item-uuid for a identifier.
-  def uuid(self, type, val):
-    return self._get( '/'.join([self.base, type, val]) )
+    # Return the item-uuid for a identifier.
+    def uuid(self, type, val):
+        return self._get('/'.join([self.base, type, val]))
 
+    # Search across all (without field) or in specific field
+    # (valid fields at http://www.loc.gov/standards/mods/mods-outline.html)
+    def search(self, q, field=None):
+        params = {'q': q}
+        if field:
+            params['field'] = field
 
-  # Search across all (without field) or in specific field 
-  # (valid fields at http://www.loc.gov/standards/mods/mods-outline.html)
-  def search(self, q, field=None):
-    params = { 'q' : q }
-    if field:
-      params['field'] = field
+        return self._get('/'.join([self.base, 'search']), params)
 
-    return self._get( '/'.join([self.base, 'search']), params )
+    # Return a mods record for a given uuid
+    def mods(self, uuid):
+        return self._get('/'.join([self.base, 'mods', uuid]))
 
+    # Generic get which handles call to api and setting of results
+    # Return: results dict
+    def _get(self, url, params=None):
+        headers = {"Authorization": "Token token=" + self.token}
+        params = params or dict()
+        params['page'] = self.page
+        params['per_page'] = self.per_page
 
-  # Return a mods record for a given uuid
-  def mods(self, uuid):
-    return self._get( '/'.join([self.base, 'mods', uuid]) )
+        r = requests.get(".".join([url, self.format]),
+                         params=params,
+                         headers=headers)
 
+        self.raw_results = r.text
+        self.results = self._to_dict(r)['nyplAPI']['response']
 
-  # Generic get which handles call to api and setting of results
-  # Return: results dict
-  def _get(self, url, params={}):
-    headers = { "Authorization" : "Token token=" + self.token }
-    params['page'] = self.page
-    params['per_page'] = self.per_page
+        if self.results['headers']['status'] == 'error':
+            self.error = {
+                'code': self.results['headers']['code'],
+                'message': self.results['headers']['message']
+            }
+        else:
+            self.error = None
 
-    r = requests.get( ".".join([url,self.format]),
-                             params=params,
-                             headers=headers)
+        return self.results
 
-    self.raw_results = r.text
-    self.results = self._to_dict(r)['nyplAPI']['response']
-
-    if self.results['headers']['status'] == 'error':
-      self.error = { 'code' : self.results['headers']['code'],
-                     'message' : self.results['headers']['message']
-                   }
-
-    return self.results
-
-
-  def _to_dict(self, r):
-    return r.json() if self.format == 'json' else xmltodict.parse(r.text)
-
+    def _to_dict(self, r):
+        return r.json() if self.format == 'json' else xmltodict.parse(r.text)
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name="nyplcollections",
-    version = "1",
-    description = "new york public library image collections api",
-    author = "nick mohoric",
-    author_email = "nick.mohoric@gmail.com",
+    version="1",
+    description="new york public library image collections api",
+    author="nick mohoric",
+    author_email="nick.mohoric@gmail.com",
     install_requires=[
         "xmltodict",
         "requests"
     ],
-    packages = ['nyplcollections'],
-    long_description = """Really long text here.""" 
-) 
+    packages=['nyplcollections'],
+    long_description="""Really long text here."""
+)
