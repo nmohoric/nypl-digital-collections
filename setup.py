87d8f780b4a8557fa3f330ca74b1e47ff9d7ba81
fitnr <fitnr@fakeisthenewreal>
2015-02-12 12:06:20 -0500

    regularize package name
    the package name in setup.py has to match the folder name, and dashes
    aren’t kosher

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 
 files = ["nyplcollections.py"]
 
-setup(name = "NYPLsearch",
+setup(
+    name="nyplcollections",
     version = "1",
     description = "new york public library image collections api",
     author = "nick mohoric",
     author_email = "nick.mohoric@gmail.com",
-    packages = ['NYPLsearch'],
     package_data = {'package' : files },
     install_requires=["xmltodict"],
+    packages = ['nyplcollections'],
     long_description = """Really long text here.""" 
 ) 
